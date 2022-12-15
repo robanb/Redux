72d500a050ad2d27de3292c40ee6dcc8cbd50430
Roba Nath Basnet <robanbth@gmail.com>
2022-12-15 21:39:42 +0600

    doing the resetup of the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import "./index.css";
-import App from "./App";
-import store from "./store/index";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<Provider store={store}>
-		<App />
-	</Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
